0618b2160328475b38a2ecb472767b431b54d32d
axunion <120321953+axunion@users.noreply.github.com>
2024-09-02 23:36:05 +0900

    Restructure vue-router to support nested routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,19 +9,31 @@ const router = createRouter({
       component: () => import('@/views/request/RequestPage.vue')
     },
     {
-      path: '/survey/2024/09',
-      name: 'survey202409',
-      component: () => import('@/views/survey/SurveyPage202409.vue')
+      path: '/survey',
+      name: 'survey',
+      children: [
+        {
+          path: '2024/09',
+          name: 'survey202409',
+          component: () => import('@/views/survey/SurveyPage202409.vue')
+        }
+      ]
     },
     {
-      path: '/entry/2024/09',
-      name: 'entry202409',
-      component: () => import('@/views/entry/EntryPage202409.vue')
-    },
-    {
-      path: '/entry/2024/02',
-      name: 'entry202402',
-      component: () => import('@/views/entry/EntryPage202402.vue')
+      path: '/entry',
+      name: 'entry',
+      children: [
+        {
+          path: '2024/09',
+          name: 'entry202409',
+          component: () => import('@/views/entry/EntryPage202409.vue')
+        },
+        {
+          path: '2024/02',
+          name: 'entry202402',
+          component: () => import('@/views/entry/EntryPage202402.vue')
+        }
+      ]
     }
   ]
 })
