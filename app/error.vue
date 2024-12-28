5f894fcca29f476d2aa29a8c0abfdd1c23f2a05d
axunion <120321953+axunion@users.noreply.github.com>
2024-12-28 21:25:26 +0900

    Add 404 Not Found error page with styled template

---
+++ app/error.vue
@@ -0,0 +1,33 @@
+<template>
+    <h1 class="h1">404 Not Found</h1>
+</template>
+
+<style>
+html {
+    background-image: linear-gradient(-225deg, #77FFD2 0%, #6297DB 48%, #1EECFF 100%);
+}
+
+body {
+    align-items: center;
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    margin: 0;
+}
+</style>
+
+<style scoped>
+.h1 {
+    color: whitesmoke;
+    font-family: "Courier New", Courier, monospace;
+    font-size: 40px;
+    font-weight: normal;
+    letter-spacing: -0.05em;
+    margin: 0;
+    text-shadow:
+        0 1px dimgray,
+        0 -1px dimgray,
+        1px 0 dimgray,
+        -1px 0 dimgray;
+}
+</style>
