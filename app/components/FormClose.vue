38ad4c6a2e1a3f6724118c504bd8df884959dfec
axunion <120321953+axunion@users.noreply.github.com>
2025-02-21 00:13:05 +0900

    Improve styling for form completion message

--- app/components/FormClose.vue
+++ app/components/FormClose.vue
@@ -1,13 +1,21 @@
 <template>
   <AppCard>
-    <SvgClose class="icon" />
-    <p>
-      <slot>このフォームは終了しています。<br />This form is now closed.</slot>
-    </p>
+    <div class="close">
+      <SvgClose class="icon" />
+
+      <p>
+        <slot>このフォームは終了しています。<br />This form is now closed.</slot>
+      </p>
+    </div>
   </AppCard>
 </template>
 
 <style scoped>
+.close {
+  padding: 5vh 0;
+  text-align: center;
+}
+
 .icon {
   height: 4em;
 }
