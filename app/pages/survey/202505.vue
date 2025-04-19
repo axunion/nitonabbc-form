aba6bc256f6fad5824360cac94dcb2a40e1ffab1
axunion <120321953+axunion@users.noreply.github.com>
2025-04-19 15:10:39 +0900

    feat: Add 202505 survey page

---
+++ app/pages/survey/202505.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+useHead({
+  title: "2025 JBBF全国青年フェローシップキャンプ アンケート",
+});
+</script>
+
+<template>
+  <HeaderDefault>
+    <template #heading>2025 JBBF全国青年フェローシップキャンプ アンケート</template>
+    <template #date>開催日：2025年4月6日〜7日</template>
+  </HeaderDefault>
+
+  <main>
+    <FormClose>
+      このアンケートは準備中です。
+    </FormClose>
+  </main>
+</template>
