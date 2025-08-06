0ccbf83affd2235642db8b9b095887b83b7e1e77
axunion <120321953+axunion@users.noreply.github.com>
2025-08-06 22:36:38 +0900

    fix: adjust layout and improve styling in ConfirmList component

--- src/pages/2025/09/_confirm-list.tsx
+++ src/pages/2025/09/_confirm-list.tsx
@@ -43,9 +43,9 @@ export default function ConfirmList() {
 	});
 
 	return (
-		<div class="px-4 py-2 overflow-x-auto">
+		<div class="py-4">
 			<Show when={confirmData.state === "pending"}>
-				<div class="min-h-40 flex justify-center items-center">
+				<div class="min-h-80 flex justify-center items-center">
 					<LoadingSpinner />
 				</div>
 			</Show>
@@ -67,47 +67,48 @@ export default function ConfirmList() {
 						</div>
 					}
 				>
-					<div
-						class={`mb-4 p-4 bg-white border border-gray-300 rounded-md ${fadeInClass}`}
-					>
+					<div class={`mx-4 p-4 bg-white rounded-md shadow ${fadeInClass}`}>
 						<div class="font-bold text-center">
-							参加費合計：{formatCurrency(totalFee())}
+							<span class="tracking-wider">参加費合計</span>
+							<span class="ml-2">{formatCurrency(totalFee())}</span>
 						</div>
 					</div>
 
-					<table
-						class={`w-full min-w-max bg-white/80 rounded shadow text-sm ${fadeInClass}`}
-					>
-						<thead>
-							<tr class="border-b border-gray-300">
-								<th class="p-3 min-w-max">名前</th>
-								<th class="w-12">夕食</th>
-								<th class="w-12">宿泊</th>
-								<th class="w-12">朝食</th>
-								<th class="w-12">昼食</th>
-								<th class="w-20">参加費</th>
-								<th class="w-60">分科会第一希望</th>
-								<th class="w-60">分科会第二希望</th>
-							</tr>
-						</thead>
-						<tbody>
-							<For each={validParticipants()}>
-								{(item) => (
-									<tr class="even:bg-gray-50 text-center">
-										<td class="p-3 text-left">{item[0]}</td>
-										<For each={item.slice(1, 5)}>
-											{(val) => <td>{val ? "○" : ""}</td>}
-										</For>
-										<td class="p-3">
-											{item[9] ? formatCurrency(item[9]) : "-"}
-										</td>
-										<td class="p-3">{item[5] || "-"}</td>
-										<td class="p-3">{item[6] || "-"}</td>
-									</tr>
-								)}
-							</For>
-						</tbody>
-					</table>
+					<div class="p-4 overflow-x-auto">
+						<table
+							class={`w-full min-w-max bg-white rounded shadow text-sm ${fadeInClass}`}
+						>
+							<thead>
+								<tr class="border-b border-gray-300">
+									<th class="p-3 min-w-max">名前</th>
+									<th class="w-12">夕食</th>
+									<th class="w-12">宿泊</th>
+									<th class="w-12">朝食</th>
+									<th class="w-12">昼食</th>
+									<th class="w-20">参加費</th>
+									<th class="w-60">分科会第一希望</th>
+									<th class="w-60">分科会第二希望</th>
+								</tr>
+							</thead>
+							<tbody>
+								<For each={validParticipants()}>
+									{(item) => (
+										<tr class="even:bg-gray-50 text-center">
+											<td class="p-3 text-left">{item[0]}</td>
+											<For each={item.slice(1, 5)}>
+												{(val) => <td>{val ? "○" : ""}</td>}
+											</For>
+											<td class="p-3">
+												{item[9] ? formatCurrency(item[9]) : "-"}
+											</td>
+											<td class="p-3">{item[5] || "-"}</td>
+											<td class="p-3">{item[6] || "-"}</td>
+										</tr>
+									)}
+								</For>
+							</tbody>
+						</table>
+					</div>
 				</Show>
 			</Show>
 		</div>
